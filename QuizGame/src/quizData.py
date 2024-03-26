392473caee21700cc7f3c824e0490c54c77231aa
Jakub-Karczewski <66319795+Jakub-Karczewski@users.noreply.github.com>
2024-03-26 14:43:00 +0100

    Update quizData.py

--- QuizGame/src/quizData.py
+++ QuizGame/src/quizData.py
@@ -6,15 +6,40 @@ quizData = [
         "category": "python"
     },
     {
-        "question": "Jakiego operatora używamy do wykonania dzielenia bez reszty?",
-        "choices": ["//", "/", "%", "**"],
-        "answer": "//",
+        "question": "Czy przypisanie całej listy do zmiennej (typu a = b) spowoduje powstanie kopii obiektu?",
+        "choices": ["Zawsze", "Nigdy", "Tylko gdy lista jest niepusta", "Zależy od typu listy"],
+        "answer": "Nigdy",
         "category": "python"
     },
     {
-        "question": "Jakiego operatora używamy do wykonania dzielenia bez reszty?",
-        "choices": ["//", "/", "%", "**"],
-        "answer": "//",
+        "question": "Jakiego operatora używamy do potęgowania?",
+        "choices": ["^", "**", "<<", "$"],
+        "answer": "**",
+        "category": "python"
+    },
+    {
+        "question": "Jakiego operatora używamy do operacji bitowej XOR?",
+        "choices": ["^", ">>", "<<", "&"],
+        "answer": "^",
+        "category": "python"
+    },
+    {
+        "question": "Jak zapisuje się operatory logiczne i, lub, nie?",
+        "choices": [ "and, or, not", "&&, ||, !", "&, |, !", "and, or, ~" ],
+        "answer": "and, or, not",
+        "category": "python"
+    },
+    {
+        "question": "Czy w Pythonie można przekazać funkcję do zmiennej?",
+        "choices": [ "TAK", "NIE", "gdy jest typu void", "gdy nie jest typu void" ],
+        "answer": "TAK",
+        "category": "python"
+    },
+    {
+        "question": "Czy w Pythonie istnieją wskaźniki?",
+        "choices": ["TAK", "NIE", "tylko referencja", "nie ma ani wskazników ani referencji"],
+        "answer": "tylko referencja",
         "category": "python"
     }
+    
 ]
