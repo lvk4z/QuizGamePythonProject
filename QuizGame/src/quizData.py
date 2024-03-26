8f570112c655ebed83eb2ff118edc16ccb2d4d0b
Jakub-Karczewski <66319795+Jakub-Karczewski@users.noreply.github.com>
2024-03-26 15:42:36 +0100

    Update quizData.py

--- QuizGame/src/quizData.py
+++ QuizGame/src/quizData.py
@@ -4,42 +4,5 @@ quizData = [
         "choices": ["//", "/", "%", "**"],
         "answer": "//",
         "category": "python"
-    },
-    {
-        "question": "Czy przypisanie całej listy do zmiennej (typu a = b) spowoduje powstanie kopii obiektu?",
-        "choices": ["Zawsze", "Nigdy", "Tylko gdy lista jest niepusta", "Zależy od typu listy"],
-        "answer": "Nigdy",
-        "category": "python"
-    },
-    {
-        "question": "Jakiego operatora używamy do potęgowania?",
-        "choices": ["^", "**", "<<", "$"],
-        "answer": "**",
-        "category": "python"
-    },
-    {
-        "question": "Jakiego operatora używamy do operacji bitowej XOR?",
-        "choices": ["^", ">>", "<<", "&"],
-        "answer": "^",
-        "category": "python"
-    },
-    {
-        "question": "Jak zapisuje się operatory logiczne i, lub, nie?",
-        "choices": [ "and, or, not", "&&, ||, !", "&, |, !", "and, or, ~" ],
-        "answer": "and, or, not",
-        "category": "python"
-    },
-    {
-        "question": "Czy w Pythonie można przekazać funkcję do zmiennej?",
-        "choices": [ "TAK", "NIE", "gdy jest typu void", "gdy nie jest typu void" ],
-        "answer": "TAK",
-        "category": "python"
-    },
-    {
-        "question": "Czy w Pythonie istnieją wskaźniki?",
-        "choices": ["TAK", "NIE", "tylko referencja", "nie ma ani wskazników ani referencji"],
-        "answer": "tylko referencja",
-        "category": "python"
     }
-    
 ]
