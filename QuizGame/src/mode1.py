8a449e29427816559900d809dbaade7a97a07c9a
Jakub Karczewski <karczewskij@student.agh.edu.pl>
2024-04-08 20:32:18 +0200

    disabling buttons after answering

--- QuizGame/src/mode1.py
+++ QuizGame/src/mode1.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import ttk, messagebox
 import json
 import random
+import time
 
 
 score = 0
@@ -9,7 +10,7 @@ current_question = 0
 f = open("data_quiz.json", encoding="utf-8")
 quiz_data = json.load(f)["data_quiz"]
 random.shuffle(quiz_data)
-#print(quiz_data)
+
 def showQuestion():
     global current_question
     question = quiz_data[current_question]
@@ -23,6 +24,16 @@ def showQuestion():
     next_button.pack_forget()
 
 
+def DisableButtons():
+    for i in range(4):
+        choice_btns[i]["state"] = "disabled"
+
+
+def EnableButtons():
+    for i in range(4):
+        choice_btns[i]["state"] = "enabled"
+
+
 def checkAnswer(choice):
     global score, current_question
     question = quiz_data[current_question]
@@ -34,6 +45,8 @@ def checkAnswer(choice):
     else:
         check_label.config(text="Zła odpowiedź", foreground="red")
     next_button.pack()
+    DisableButtons()
+
 
 
 def nextQuestion():
@@ -62,7 +75,7 @@ def mainT1(frame):
     for i in range(4):
         button = ttk.Button(
             root,
-            command=lambda i=i: checkAnswer(i)
+            command=lambda k=i: checkAnswer(k)
         )
         button.pack(pady=5)
         choice_btns.append(button)
