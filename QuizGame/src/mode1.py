5d1684b78305fb4ec0b89b7039c4c3c465a7a944
lvk4z <147399994+lvk4z@users.noreply.github.com>
2024-05-19 22:22:45 +0200

    Changing to pygame cd4

--- QuizGame/src/mode1.py
+++ QuizGame/src/mode1.py
@@ -2,13 +2,33 @@ import sys
 import pandas
 import pygame
 from pygame.locals import *
-from utils import drawText, drawProgressBar
+from utils import  show_friends, draw_background, draw_lifebuoys, draw_options, draw_question, draw_score_table,draw_timer
+import random
 
 OLIVE_GREEN = (193, 203, 15)
+def use_50_50(abcd):
+        incorrect_answers = [i for i, ans in enumerate(abcd) if not ans[1]]
+        return random.sample(incorrect_answers, 2)
 
 def mode1_play(window, width):
     bg_img = pygame.image.load('QuizGame/src/resources/game/bg.jpg').convert_alpha()
     option_hover = pygame.image.load('QuizGame/src/resources/game/answer_hover.png').convert_alpha()
+    score_table = [pygame.image.load('QuizGame/src/resources/game/score_table1.jpg').convert_alpha(),
+                    pygame.image.load('QuizGame/src/resources/game/score_table2.jpg').convert_alpha(),
+                    pygame.image.load('QuizGame/src/resources/game/score_table3.jpg').convert_alpha(),
+                    pygame.image.load('QuizGame/src/resources/game/score_table4.jpg').convert_alpha(),
+                    pygame.image.load('QuizGame/src/resources/game/score_table5.jpg').convert_alpha(),
+                    pygame.image.load('QuizGame/src/resources/game/score_table6.jpg').convert_alpha(),
+                    pygame.image.load('QuizGame/src/resources/game/score_table7.jpg').convert_alpha(),
+                    pygame.image.load('QuizGame/src/resources/game/score_table8.jpg').convert_alpha(),
+                    pygame.image.load('QuizGame/src/resources/game/score_table9.jpg').convert_alpha(),
+                    pygame.image.load('QuizGame/src/resources/game/score_table10.jpg').convert_alpha(),
+                    pygame.image.load('QuizGame/src/resources/game/score_table11.jpg').convert_alpha(),
+                    pygame.image.load('QuizGame/src/resources/game/score_table12.jpg').convert_alpha()]
+    
+    lifebuoy_50 =  pygame.image.load('QuizGame/src/resources/game/lifebuoy_50.png').convert_alpha()
+    lifebuoy_friend =  pygame.image.load('QuizGame/src/resources/game/lifebuoy_friend.png').convert_alpha()
+    lifebuoy_time =  pygame.image.load('QuizGame/src/resources/game/lifebuoy_time.png').convert_alpha()
 
     q_base_easy = pandas.read_excel('QuizGame/src/resources/questionsBase.xlsx', "easy", usecols = "A,B,C,D,E,F").to_dict('index')
     q_base_medium = pandas.read_excel('QuizGame/src/resources/questionsBase.xlsx', "medium", usecols = "A,B,C,D,E,F").to_dict('index')
@@ -21,64 +41,47 @@ def mode1_play(window, width):
     question_number = 0
     running = True
     load_next_question = True
+    used_lifebuoy_50 = False
+    used_lifebuoy_friend = False
+    used_lifebuoy_time = False
     start_time = pygame.time.get_ticks()
+    hidden_answers = []
+
+    
+
     while running:
         mouse_pointer = pygame.mouse.get_pos()
-        time_left = full_time - (pygame.time.get_ticks()-start_time)/1000
-        if int(time_left) >= 0:
-            text = timerfont.render(str(int(time_left)), True, (184, 193, 209))
-        window.blit(bg_img,(0,0))
-        text_rect = text.get_rect(center=(width // 2, 200))
-        window.blit(text, text_rect)
+        time_left = full_time - (pygame.time.get_ticks() - start_time) / 1000
 
-        if time_left > 0.1:
-            progress_rect = pygame.Rect(520, 330, 240, 16)
-            drawProgressBar(window, progress_rect, time_left / 30)
+        if  int(time_left) <= 0 or question_number >= 12:
+            sys.exit()
+            running = False
+
+        draw_background(window, bg_img)
+        draw_timer(window, timerfont, time_left, width)
+        draw_score_table(window, score_table, question_number)
 
         if load_next_question:
+            hidden_answers = []
             if question_number <= 3:
                 Q = q_base_easy[question_number]["PYTANIE"]
-                ABCD = [(q_base_easy[question_number]["A"], True), (q_base_easy[question_number]["B"], False), (q_base_easy[question_number]["C"], False), (q_base_easy[question_number]["D"], False)]
+                ABCD = [(q_base_easy[question_number]["A"], True), (q_base_easy[question_number]["B"], False),
+                        (q_base_easy[question_number]["C"], False), (q_base_easy[question_number]["D"], False)]
                 load_next_question = False
             elif question_number <= 7:
                 Q = q_base_medium[question_number - 4]["PYTANIE"]
-                ABCD = [(q_base_medium[question_number - 4]["A"], True), (q_base_medium[question_number - 4]["B"], False), (q_base_medium[question_number - 4]["C"], False), (q_base_medium[question_number - 4]["D"], False)]
+                ABCD = [(q_base_medium[question_number - 4]["A"], True), (q_base_medium[question_number - 4]["B"], False),
+                        (q_base_medium[question_number - 4]["C"], False), (q_base_medium[question_number - 4]["D"], False)]
                 load_next_question = False
             else:
                 Q = q_base_hard[question_number - 8]["PYTANIE"]
-                ABCD = [(q_base_hard[question_number - 8]["A"], True), (q_base_hard[question_number - 8]["B"], False), (q_base_hard[question_number - 8]["C"], False), (q_base_hard[question_number - 8]["D"], False)]
+                ABCD = [(q_base_hard[question_number - 8]["A"], True), (q_base_hard[question_number - 8]["B"], False),
+                        (q_base_hard[question_number - 8]["C"], False), (q_base_hard[question_number - 8]["D"], False)]
                 load_next_question = False
 
-        Q_frame = pygame.Rect(240, 430, 1045 - 240, 480 - 420)
-        drawText(window, Q, "white", Q_frame, qafont, True)
-
-        optionA_frame = pygame.Rect(210, 560, 370, 30)
-        if 206 < mouse_pointer[0] < 610 and 548 < mouse_pointer[1] < 598:
-            window.blit(option_hover, (208, 548))
-            drawText(window, ABCD[0][0], "black", optionA_frame, qafont, True)
-        else:
-            drawText(window, ABCD[0][0], "white", optionA_frame, qafont, True)
-
-        optionB_frame = pygame.Rect(690, 560, 370, 30)
-        if 675 < mouse_pointer[0] < 1076 and 548 < mouse_pointer[1] < 598:
-            window.blit(option_hover, (670, 548))
-            drawText(window, ABCD[1][0], "black", optionB_frame, qafont, True)
-        else:
-            drawText(window, ABCD[1][0], "white", optionB_frame, qafont, True)
-
-        optionC_frame = pygame.Rect(210, 630, 370, 30)
-        if 206 < mouse_pointer[0] < 610 and 620 < mouse_pointer[1] < 665:
-            window.blit(option_hover, (206, 619))
-            drawText(window, ABCD[2][0], "black", optionC_frame, qafont, True)
-        else:
-            drawText(window, ABCD[2][0], "white", optionC_frame, qafont, True)
-
-        optionD_frame = pygame.Rect(690, 630, 370, 30)
-        if 675 < mouse_pointer[0] < 1076 and 620 < mouse_pointer[1] < 665:
-            window.blit(option_hover, (674, 620))
-            drawText(window, ABCD[3][0], "black", optionD_frame, qafont, True)
-        else:
-            drawText(window, ABCD[3][0], "white", optionD_frame, qafont, True)
+        draw_question(window, Q, qafont)
+        draw_options(window, ABCD, qafont, mouse_pointer, option_hover, hidden_answers)
+        draw_lifebuoys(window, lifebuoy_50, lifebuoy_time, lifebuoy_friend, used_lifebuoy_50, used_lifebuoy_time, used_lifebuoy_friend)
 
         pygame.display.update()
 
@@ -87,37 +90,48 @@ def mode1_play(window, width):
                 running = False
             if event.type == MOUSEBUTTONDOWN:
                 if event.button == 1:
-                    if 206 < mouse_pointer[0] < 610 and 548 < mouse_pointer[1] < 598:
+                    if 206 < mouse_pointer[0] < 610 and 548 < mouse_pointer[1] < 598 and 0 not in hidden_answers:
                         question_number += 1
                         start_time = pygame.time.get_ticks()
                         full_time = 30
-                        if ABCD[0][1] == 'F':
+                        if not ABCD[0][1]:
                             sys.exit()
                         else:
                             load_next_question = True
-                    if 675 < mouse_pointer[0] < 1076 and 548 < mouse_pointer[1] < 598:
+                    if 675 < mouse_pointer[0] < 1076 and 548 < mouse_pointer[1] < 598 and 1 not in hidden_answers:
                         question_number += 1
                         start_time = pygame.time.get_ticks()
                         full_time = 30
-                        if ABCD[1][1] == 'F':
+                        if not ABCD[1][1]:
                             sys.exit()
                         else:
                             load_next_question = True
-                    if 206 < mouse_pointer[0] < 610 and 620 < mouse_pointer[1] < 665:
-                        start_time = pygame.time.get_ticks()
-                        full_time = 30
-                        if ABCD[2][1] == 'F':
-                            sys.exit()
-                        else:
-                            load_next_question = True
-                    if 675 < mouse_pointer[0] < 1076 and 620 < mouse_pointer[1] < 665:
+                    if 206 < mouse_pointer[0] < 610 and 620 < mouse_pointer[1] < 665 and 2 not in hidden_answers:
                         question_number += 1
                         start_time = pygame.time.get_ticks()
                         full_time = 30
-                        if ABCD[3][1] == 'F':
+                        if not ABCD[2][1]:
+                            sys.exit()
+                        else:
+                            load_next_question = True
+                    if 675 < mouse_pointer[0] < 1076 and 620 < mouse_pointer[1] < 665 and 3 not in hidden_answers:
+                        question_number += 1
+                        start_time = pygame.time.get_ticks()
+                        full_time = 30
+                        if not ABCD[3][1]:
                             sys.exit()
                         else:
                             load_next_question = True
 
-                    if 1106 < mouse_pointer[0] < 1249 and 12 < mouse_pointer[1] < 36:
+                    if 520 < mouse_pointer[0] < 590 and 310 < mouse_pointer[1] < 380 and not used_lifebuoy_50:
+                        used_lifebuoy_50 = True
+                        hidden_answers = use_50_50(ABCD)
+                    if 600 < mouse_pointer[0] < 670 and 310 < mouse_pointer[1] < 380 and not used_lifebuoy_time:
+                        used_lifebuoy_time = True
+                        full_time += 30
+                    if 680 < mouse_pointer[0] < 750 and 310 < mouse_pointer[1] < 380 and not used_lifebuoy_friend:
+                        used_lifebuoy_friend = True
+                        show_friends()
+
+                    if 1215 < mouse_pointer[0] < 1280 and 0 < mouse_pointer[1] < 62:
                         sys.exit()
--- QuizGame/src/utils.py
+++ QuizGame/src/utils.py
@@ -1,61 +1,98 @@
 # drawing_utils.py
 import pygame
+def draw_background(window, bg_img):
+    window.blit(bg_img, (0, 0))
 
-GREEN = (193, 203, 15)
-BLACK = (0, 0, 0)
+def draw_timer(window, timerfont, time_left, width):
+    text = timerfont.render(str(int(time_left)), True, (184, 193, 209))
+    text_rect = text.get_rect(center=(width // 2, 200))
+    window.blit(text, text_rect)
 
-def drawProgressBar(surface, rect, progress):
-    filled_rect = pygame.Rect(rect.left, rect.top, rect.width * progress, rect.height)
-    pygame.draw.rect(surface, GREEN, filled_rect)
-    pygame.draw.rect(surface, BLACK, rect, 2)
+def draw_question(window, question, qafont):
+    Q_frame = pygame.Rect(240, 430, 805, 50)
+    draw_text(window, question, "white", Q_frame, qafont, True)
 
-textAlignLeft = 0
-textAlignRight = 1
-textAlignCenter = 2
-textAlignBlock = 3
+def draw_options(window, ABCD, qafont, mouse_pointer, option_hover, hidden_answers):
+    option_frames = [
+        pygame.Rect(210, 560, 370, 30),
+        pygame.Rect(690, 560, 370, 30),
+        pygame.Rect(210, 630, 370, 30),
+        pygame.Rect(690, 630, 370, 30)
+    ]
 
-def drawText(surface, text, color, rect, font, aa=True, bkg=None):
-    lineSpacing = -2
-    spaceWidth, fontHeight = font.size(" ")[0], font.size("Tg")[1]
+    hover_conditions = [
+        (206 < mouse_pointer[0] < 610 and 548 < mouse_pointer[1] < 598),
+        (675 < mouse_pointer[0] < 1076 and 548 < mouse_pointer[1] < 598),
+        (206 < mouse_pointer[0] < 610 and 620 < mouse_pointer[1] < 665),
+        (675 < mouse_pointer[0] < 1076 and 620 < mouse_pointer[1] < 665)
+    ]
 
-    listOfWords = str(text).split(" ")
+    for i, frame in enumerate(option_frames):
+        if i not in hidden_answers:
+            if hover_conditions[i]:
+                window.blit(option_hover, (frame.x - 2, frame.y - 12))
+                draw_text(window, ABCD[i][0], "black", frame, qafont, True)
+            else:
+                draw_text(window, ABCD[i][0], "white", frame, qafont, True)
+
+def draw_lifebuoys(window, lifebuoy_50, lifebuoy_time, lifebuoy_friend, used_lifebuoy_50, used_lifebuoy_time, used_lifebuoy_friend):
+    if not used_lifebuoy_50:
+        window.blit(lifebuoy_50, (520, 310))
+    if not used_lifebuoy_time:
+        window.blit(lifebuoy_time, (600, 310))
+    if not used_lifebuoy_friend:
+        window.blit(lifebuoy_friend, (680, 310))
+
+def draw_score_table(window, score_table, question_number):
+    if question_number <= 12:
+        window.blit(score_table[question_number], (5, 5))
+
+def show_friends():
+    pass
+
+def draw_text(surface, text, color, rect, font, aa=True, bkg=None):
+    line_spacing = -2
+    space_width, font_height = font.size(" ")[0], font.size("Tg")[1]
+
+    list_of_words = str(text).split(" ")
     if bkg:
-        imageList = [font.render(word, 1, color, bkg) for word in listOfWords]
-        for image in imageList: image.set_colorkey(bkg)
+        image_list = [font.render(word, 1, color, bkg) for word in list_of_words]
+        for image in image_list: image.set_colorkey(bkg)
     else:
-        imageList = [font.render(word, aa, color) for word in listOfWords]
+        image_list = [font.render(word, aa, color) for word in list_of_words]
 
-    maxLen = rect[2]
-    lineLenList = [0]
-    lineList = [[]]
-    for image in imageList:
+    max_len = rect[2]
+    line_len_list = [0]
+    line_list = [[]]
+    for image in image_list:
         width = image.get_width()
-        lineLen = lineLenList[-1] + len(lineList[-1]) * spaceWidth + width
-        if len(lineList[-1]) == 0 or lineLen <= maxLen:
-            lineLenList[-1] += width
-            lineList[-1].append(image)
+        line_len = line_len_list[-1] + len(line_list[-1]) * space_width + width
+        if len(line_list[-1]) == 0 or line_len <= max_len:
+            line_len_list[-1] += width
+            line_list[-1].append(image)
         else:
-            lineLenList.append(width)
-            lineList.append([image])
+            line_len_list.append(width)
+            line_list.append([image])
 
-    lineBottom = rect[1]
-    lastLine = 0
-    for lineLen, lineImages in zip(lineLenList, lineList):
-        lineLeft = rect[0]
-        lineLeft += (rect[2] - lineLen - spaceWidth * (len(lineImages)-1)) // 2
+    line_bottom = rect[1]
+    last_line = 0
+    for line_len, line_images in zip(line_len_list, line_list):
+        line_left = rect[0]
+        line_left += (rect[2] - line_len - space_width * (len(line_images)-1)) // 2
         
-        if lineBottom + fontHeight > rect[1] + rect[3]:
+        if line_bottom + font_height > rect[1] + rect[3]:
             break
-        lastLine += 1
-        for i, image in enumerate(lineImages):
-            x, y = lineLeft + i*spaceWidth, lineBottom
+        last_line += 1
+        for i, image in enumerate(line_images):
+            x, y = line_left + i*space_width, line_bottom
             surface.blit(image, (round(x), y))
-            lineLeft += image.get_width() 
-        lineBottom += fontHeight + lineSpacing
+            line_left += image.get_width() 
+        line_bottom += font_height + line_spacing
 
-    if lastLine < len(lineList):
-        drawWords = sum([len(lineList[i]) for i in range(lastLine)])
-        remainingText = ""
-        for text in listOfWords[drawWords:]: remainingText += text + " "
-        return remainingText
+    if last_line < len(line_list):
+        draw_words = sum([len(line_list[i]) for i in range(last_line)])
+        remaining_text = ""
+        for text in list_of_words[draw_words:]: remaining_text += text + " "
+        return remaining_text
     return ""
+
